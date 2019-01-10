4b9d893f560a0d39acd08dcaca7a7c123652f1da
Oleksii Kulikov <yeexel@gmail.com>
2019-01-11 00:29:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Made with love by yeexel in Jan 2019 :)
+            Made with love by yeexel in January 2019 :) Stay tuned for more!
           </a>
         </header>
       </div>
